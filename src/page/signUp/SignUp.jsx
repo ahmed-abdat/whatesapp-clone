9493c0ac13335125d345f142384600a53822ad68
ahmed-abdat <ahmedeabdat@gmail.com>
2023-04-07 14:00:36 +0000

    fix bug

--- src/page/signUp/SignUp.jsx
+++ src/page/signUp/SignUp.jsx
@@ -15,61 +15,61 @@ import { useNavigate } from "react-router-dom";
 import useSignUp from "../../store/useSignUp";
 
 export default function SignUp() {
-  const [phone, setPhone] = useState("");
-  const [isLoading, setIsLoding] = useState(false);
+  // const [phone, setPhone] = useState("");
+  // const [isLoading, setIsLoding] = useState(false);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const setconfirmationResult = useSignUp((state) => state.setconfirmationResult);
+  // const setconfirmationResult = useSignUp((state) => state.setconfirmationResult);
 
-  // recapter form
-  const requestRecaptcha = () => {
-    window.recaptchaVerifier = new RecaptchaVerifier(
-      "sign-in-recaptcha",
-      {
-        size: "invisible",
-        callback: (response) => {
-          // reCAPTCHA solved, allow signInWithPhoneNumber.
-          sendOtp();
-          console.log(response);
-        },
-        "expired-callback": () => {
-          // Response expired. Ask user to solve reCAPTCHA again.
-          toast.error("حاول مرة أخرى");
-        },
-      },
-      auth
-    );
-  };
-  // send otp to phone number
-  const sendOtp = () => {
-    console.log("send otp");
-    const phoneNumber = `+${phone}`;
-    const appVerifier = window.recaptchaVerifier;
-    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-      .then((confirmationResult) => {
-        window.confirmationResult = confirmationResult;
-        setconfirmationResult(confirmationResult);
-        navigate('/otp')
-        toast.success(`تم إرسال رمز التحقق إلى الرقم +${phone}`);
-        setIsLoding(false);
-      })
-      .catch((error) => {
-        setIsLoding(false);
-        toast.error("حدث خطأ أثناء إرسال الرسالة");
-        console.error(error);
-      });
-  };
+  // // recapter form
+  // const requestRecaptcha = () => {
+  //   window.recaptchaVerifier = new RecaptchaVerifier(
+  //     "sign-in-recaptcha",
+  //     {
+  //       size: "invisible",
+  //       callback: (response) => {
+  //         // reCAPTCHA solved, allow signInWithPhoneNumber.
+  //         sendOtp();
+  //         console.log(response);
+  //       },
+  //       "expired-callback": () => {
+  //         // Response expired. Ask user to solve reCAPTCHA again.
+  //         toast.error("حاول مرة أخرى");
+  //       },
+  //     },
+  //     auth
+  //   );
+  // };
+  // // send otp to phone number
+  // const sendOtp = () => {
+  //   console.log("send otp");
+  //   const phoneNumber = `+${phone}`;
+  //   const appVerifier = window.recaptchaVerifier;
+  //   signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+  //     .then((confirmationResult) => {
+  //       window.confirmationResult = confirmationResult;
+  //       setconfirmationResult(confirmationResult);
+  //       navigate('/otp')
+  //       toast.success(`تم إرسال رمز التحقق إلى الرقم +${phone}`);
+  //       setIsLoding(false);
+  //     })
+  //     .catch((error) => {
+  //       setIsLoding(false);
+  //       toast.error("حدث خطأ أثناء إرسال الرسالة");
+  //       console.error(error);
+  //     });
+  // };
 
-  // handel submit form
-  const handelSumbit = (e) => {
-    e.preventDefault();
-    if (phone.length >= 11) {
-      setIsLoding(true);
-      requestRecaptcha();
-      sendOtp();
-    }
-  };
+  // // handel submit form
+  // const handelSumbit = (e) => {
+  //   e.preventDefault();
+  //   if (phone.length >= 11) {
+  //     setIsLoding(true);
+  //     requestRecaptcha();
+  //     sendOtp();
+  //   }
+  // };
 
   // sign up with google
   const signInWithGoogle = () => {
