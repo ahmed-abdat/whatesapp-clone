2cce7e8c3051b3a9096de3c464b6cc8f821a8e09
ahmed-abdat <ahmedeabdat@gmail.com>
2023-04-16 16:01:46 +0000

    updated\

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,8 @@
 import HomepageSearch from "./HomePageSearch";
 import HomePageUser from "./HomePageUser";
-// import UserProfile from "./UserProfile";
 import HomePageHeader from "./HomePageHeader";
 import useUser from "../store/useUser";
 import { useEffect, useState } from "react";
-import { collection, query, where, onSnapshot, limit } from "firebase/firestore";
-import { db } from "../config/firebase";
 import { lazy } from "react";
 import { Suspense } from "react";
 import SpinerLoader from "./SpinerLoader";
@@ -27,23 +24,23 @@ export default function HomePage() {
   const isLogoutLoading = useUser((state) => state.isLogoutLoading);
 
   // get all user in firebase except the current user
-  useEffect(() => {
-    setIsLoading(true);
-    const q = query(
-      collection(db, "users"),
-      where("uid", "!=", currentUser.uid),
-      limit(10)
-    );
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const usereData = [];
-      querySnapshot.forEach((doc) => {
-        usereData.push({ ...doc.data(), id: doc.id });
-      });
-      setAllUsers(usereData);
-      setIsLoading(false);
-    });
-    return () => unsubscribe();
-  }, []);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   const q = query(
+  //     collection(db, "users"),
+  //     where("uid", "!=", currentUser.uid),
+  //     limit(10)
+  //   );
+  //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  //     const usereData = [];
+  //     querySnapshot.forEach((doc) => {
+  //       usereData.push({ ...doc.data(), id: doc.id });
+  //     });
+  //     setAllUsers(usereData);
+  //     setIsLoading(false);
+  //   });
+  //   return () => unsubscribe();
+  // }, []);
 
   return (
     <div className="home-page">
@@ -62,7 +59,7 @@ export default function HomePage() {
           {/* home page search */}
           <HomepageSearch />
           {/* home page user profile */}
-          <div className="user-profile--container">
+          {/* <div className="user-profile--container">
             {!isLoading ? (
               allUsers.map((user) => {
                 return <HomePageUser key={user.id} {...user} />
@@ -70,7 +67,7 @@ export default function HomePage() {
             ) : (
               <SpinerLoader />
             )}
-          </div>
+          </div> */}
         </>
       )}
     </div>
