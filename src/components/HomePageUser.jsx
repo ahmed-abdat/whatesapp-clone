cf12cbc44524b5aa094e85aad38fd798f7b04db9
ahmed-abdat <ahmedeabdat@gmail.com>
2023-04-17 13:11:29 +0000

    new fix

--- src/components/HomePageUser.jsx
+++ src/components/HomePageUser.jsx
@@ -6,11 +6,11 @@ import defaultAvatar from '../assets/img/default-avatar.svg'
 
 export default function HomePageUser({ displayName, photoURL, isOnline, lastSeen }) {
   moment.locale('ar');
-  const [timeAgo, setTimeAgo] = useState(moment(lastSeen).fromNow("DD/MM/YYYY, hh:mm A"));
+  const [timeAgo, setTimeAgo] = useState(moment(lastSeen).locale('ar').fromNow("DD/MM/YYYY, hh:mm A"));
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimeAgo(moment(lastSeen).fromNow("DD/MM/YYYY, hh:mm A"));
+      setTimeAgo(moment(lastSeen).locale('ar').fromNow("DD/MM/YYYY, hh:mm A"));
     }, 10000); 
 
     return () => clearInterval(interval);
